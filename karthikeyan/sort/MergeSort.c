19a8c63cdd5e67221b6187b4402c8c9e190a983c
karthi keyan N <Karthikeyan.N@anorasolutions.com>
2025-04-22 16:43:48 +0530

    Remove main in merge sort

--- karthikeyan/sort/MergeSort.c
+++ karthikeyan/sort/MergeSort.c
@@ -66,15 +66,3 @@ void mergeSort(int arr[], int left, int right) {
     }
 }
 
-int main() {
-    
-    int arr[] = { 12, 11, 13, 5, 6, 7 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-	
-  	// Sorting arr using mergesort
-    mergeSort(arr, 0, n - 1);
-
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    return 0;
-}
